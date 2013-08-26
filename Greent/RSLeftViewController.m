64966ce165684c5abba05be2dc511a283b41685b
R0CKSTAR <yeahdongcn@gmail.com>
2013-08-26 15:53:45 +0800

    Add brands.json for the left slide

--- Greent/RSLeftViewController.m
+++ Greent/RSLeftViewController.m
@@ -29,11 +29,7 @@
 {
     [super viewDidLoad];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
 }
 
 - (void)didReceiveMemoryWarning
